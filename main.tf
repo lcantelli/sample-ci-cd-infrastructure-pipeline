01738ac636da279a6ccc341004d6b72d7444f0d9
Lucas Cantelli <lcantelli@x24factory.com>
2022-08-13 11:50:09 +0200

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc" "example_vcp" {
+  cidr_block = "10.10.10.0/24"
+
+  tags = {
+    Name = "Test VPC"
+    ProvisionedBy  = "Terraform"
+  }
+}
+
+resource "aws_subnet" "example_subnet1" {
+  vpc_id     = aws_vpc.example_vcp.id
+  cidr_block = "10.10.10.0/25"
+
+  tags = {
+    Name = "Test Subnet 1"
+    ProvisionedBy  = "Terraform"
+  }
+}
+
+resource "aws_subnet" "example_subnet2" {
+  vpc_id     = aws_vpc.example_vcp.id
+  cidr_block = "10.10.10.128/25"
+
+  tags = {
+    Name = "Test Subnet 2"
+    ProvisionedBy  = "Terraform"
+  }
+}
